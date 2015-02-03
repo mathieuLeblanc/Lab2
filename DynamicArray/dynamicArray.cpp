468e2f751a7f131af4a3cba749e6c5c82800108f
mathieuLeblanc <mathieu_leblancc@hotmail.fr>
2015-02-03 10:34:27 -0500

    quelques test fonctionnent

--- DynamicArray/dynamicArray.cpp
+++ DynamicArray/dynamicArray.cpp
@@ -1,1 +1,67 @@
 #include "dynamicArray.h"
+
+DynamicArray::DynamicArray(int _capacite)
+{
+	if (_capacite < 1) throw invalid_argument("pas bien");
+	capacite = _capacite;
+	tabElements = new int[_capacite];
+	for (int i = 0; i < capacite; ++i){
+		tabElements[i] = 0;
+	}
+
+}
+
+DynamicArray::~DynamicArray()
+{
+	delete[] tabElements;
+}
+
+unsigned int DynamicArray::getCapacite()
+{
+	return capacite;
+}
+
+void DynamicArray::setElement(unsigned int _index, int _valeur)
+{
+	
+	if (_index >= capacite)
+	{
+		setCapacite(_index + 1);
+		
+	}
+	
+	tabElements[_index] = _valeur;
+	
+
+}
+
+int DynamicArray::getElement(unsigned int _index)
+{
+	return tabElements[_index];
+}
+
+void DynamicArray::setCapacite(unsigned int _capacite)
+
+{
+	if (_capacite < 1)
+	{
+		throw invalid_argument("pas bien");
+	}
+	else{
+
+		int *tempTab = new int[_capacite];
+		for (int i = 0; i < _capacite; ++i){
+			if (i < capacite){
+				tempTab[i] = tabElements[i];
+			}
+			else{
+				tempTab[i] = 0;
+			}
+
+		}
+		capacite = _capacite;
+		delete[] tabElements;
+		tabElements = tempTab;
+	}
+	
+}
